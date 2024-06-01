acddbfd7c6367fd8c0ad2f875db978a18f4a693e
guimiiller <guilhermemillerblack@gmail.com>
2024-06-01 17:58:45 -0300

    add gallery

--- js/script.js
+++ js/script.js
@@ -65,3 +65,30 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 /* BORDA */
+
+// JavaScript para modal da galeria de fotos
+document.addEventListener('DOMContentLoaded', function () {
+    var modal = document.getElementById("modal");
+    var modalImg = document.getElementById("modal-img");
+    var captionText = document.getElementById("caption");
+    var span = document.getElementsByClassName("close")[0];
+
+    var galleryItems = document.querySelectorAll('.gallery_item img');
+    galleryItems.forEach(function(item) {
+        item.addEventListener('click', function() {
+            modal.style.display = "block";
+            modalImg.src = this.src;
+            captionText.innerHTML = this.alt;
+        });
+    });
+
+    span.onclick = function() {
+        modal.style.display = "none";
+    };
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    };
+});
